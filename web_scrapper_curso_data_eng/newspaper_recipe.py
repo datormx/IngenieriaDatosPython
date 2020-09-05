4135aa1d00b8fc5cba740b7c8fcd1b9141a83c72
Miguel Torres <migueltorresio@outlook.com>
2020-09-04 22:11:45 -0500

    Se hizo limpieza adicional de los datos en el body y generando un uid. Se agregaron los pasos al recipe.

--- web_scrapper_curso_data_eng/newspaper_recipe.py
+++ web_scrapper_curso_data_eng/newspaper_recipe.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 logging.basicConfig(level = logging.INFO)
 from urllib.parse import urlparse
+import hashlib
 
 import pandas as pd
 
@@ -15,6 +16,9 @@ def run(filename):
     newspaper_uid = _extract_newspaper_uid(filename)
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
+    df = _fill_missing_titles(df)
+    df = _generate_uids_for_rows(df)
+    df = _remove_new_lines_from_body(df)
 
     return df
 
@@ -43,6 +47,48 @@ def _add_newspaper_uid_column(df, newspaper_uid):
 def _extract_host(df):
     logger.info('Extracting host from urls...')
     df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
+
+    return df
+
+
+def _fill_missing_titles(df):
+    logger.info('Filling missing titles...')
+    missing_titles_mask = df['title'].isna()
+
+    missing_titles = (df[missing_titles_mask]['url']
+                        .str.extract(r'(?P<missing_titles>[^/]+)')
+                        .applymap(lambda title: title.split('-'))
+                        .applymap(lambda title_word_list: ' '.join(title_word_list))
+                     )
+
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
+
+    return df
+
+
+def _generate_uids_for_rows(df):
+    logger.info('Generatind uids for each row...')
+    uids = (df
+                .apply(lambda row: hashlib.md5(bytes(row['url'].encode())), axis=1)
+                .apply(lambda hash_object: hash_object.hexdigest())
+            )
+
+    df['uid'] = uids
+
+    return df.set_index('uid')
+
+
+def _remove_new_lines_from_body(df):
+    logger.info('Remove new lines from body')
+
+    stripped_body = (df
+                        .apply(lambda row: row['body'], axis=1)
+                        .apply(lambda body: list(body))
+                        .apply(lambda letters: list(map(lambda letter: letter.replace('\n', ''), letters)))
+                        .apply(lambda letters: ''.join(letters))
+                    )
+
+    df['body'] = stripped_body
 
     return df
 
